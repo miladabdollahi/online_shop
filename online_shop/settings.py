e90400ec2363b6cfe68eb38e134f1832f243c51e
milad <miladabdollahi1397@gmail.com>
2020-07-24 09:21:11 +0430

    add account and extends default user django

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Authentication backends
+# https://docs.djangoproject.com/en/3.0/ref/settings/#authentication-backends
+
+AUTHENTICATION_BACKENDS = ['account.auth.backends.EmailPhoneBackend']
+
+
+# Substituting a custom User model
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-user-model
+
+AUTH_USER_MODEL = 'account.User'
 
 
 # Internationalization
