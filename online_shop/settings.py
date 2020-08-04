8ec74f74dea80b03f55b76fda7e07c1c1a7fdec0
milad <miladabdollahi1397@gmail.com>
2020-08-04 16:53:33 +0430

    add swagger for documentation api

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'account',
     'phonenumber_field',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -94,7 +95,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
 }
 
 
